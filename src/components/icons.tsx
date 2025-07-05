d2a3ed3d0f5e7b8431ca5291982d72a1cb5645d7
Abdulwahed Mansour <abdulwahed.mansour@gmail.com>
2025-07-05 13:15:27 +0000

    fix icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -2,7 +2,7 @@ import type { SVGProps } from "react";
 
 export function IconRust(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 250" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 250" role="img" aria-hidden="true" {...props}>
       <path fill="#DEA584" d="M39 99.3L125 41v168l-86-57.7z" />
       <path fill="#000" d="M39 99.3L125 41v26.5L62 108v34l63-40.3V142L39 99.3z" />
       <path fill="#DEA584" d="M211 150.7L125 209V41l86 58.3z" />
@@ -17,7 +17,7 @@ export function IconRust(props: SVGProps<SVGSVGElement>) {
 
 export function IconPython(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true" {...props}>
       <g fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
         <path fill="#3776AB" d="M12 21a9 9 0 1 0-9-9c0 1.488.36 2.892 1 4.127V12a2 2 0 1 1 4 0v5a2 2 0 1 1-4 0v-1.127A9.011 9.011 0 0 0 12 21Z" />
         <path fill="#FFD43B" d="M12 3a9 9 0 1 0 9 9c0-1.488-.36-2.892-1-4.127V12a2 2 0 1 1-4 0V7a2 2 0 1 1 4 0v1.127A9.011 9.011 0 0 0 12 3Z" />
